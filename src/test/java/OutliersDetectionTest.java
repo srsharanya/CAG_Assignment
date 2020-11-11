5133c0e2e5c8076ca6ac7a6335eb0e9ff6c18568
SHARANYA <srsharanya@gmail.com>
2020-11-12 00:49:20 +0530

    clean up

--- src/test/java/OutliersDetectionTest.java
+++ src/test/java/OutliersDetectionTest.java
@@ -1,7 +1,5 @@
-import jdk.internal.jline.internal.TestAccessible;
 import org.junit.Assert;
 import org.junit.Test;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
